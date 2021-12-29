426b9a3d9f7a7bd35122a348fe231e26236019bc
gettsu <tsuge-daisuke430@g.ecc.u-tokyo.ac.jp>
2021-12-29 10:54:09 +0900

    [update] DiskManager

--- src/disk.rs
+++ src/disk.rs
@@ -1,10 +1,50 @@
+use std::convert::TryInto;
 use std::fs::{File, OpenOptions};
 use std::io::{self, prelude::*, SeekFrom};
 use std::path::Path;
 
+use zerocopy::{AsBytes, FromBytes};
+
 pub const PAGE_SIZE: usize = 4096;
 
+#[derive(Debug, Clone, Copy, Eq, PartialEq, Hash, FromBytes, AsBytes)]
+#[repr(C)]
 pub struct PageId(pub u64);
+impl PageId {
+    pub const INVALID_PAGE_ID: PageId = PageId(u64::MAX);
+
+    pub fn valid(self) -> Option<PageId> {
+        if self == Self::INVALID_PAGE_ID {
+            None
+        }else {
+            Some(self)
+        }
+    }
+
+    pub fn to_u64(self) -> u64 {
+        self.0
+    }
+}
+
+impl Default for PageId {
+    fn default()->self {
+        Self::INVALID_PAGE_ID
+    }
+}
+
+impl From<Option<PageId>> for PageId {
+    fn from(page_id: Option<PageId>) -> Self {
+        page_id.unwrap_or_default();
+    }
+}
+
+impl From<&[u8]> for PageId {
+    fn from(bytes: &[u8]) -> Self {
+        let arr = bytes.try_into().unwrap();
+        PageId(u64::from_ne_bytes(arr))
+    }
+}
+
 pub struct DiskManager {
     heap_file: File,
     next_page_id: u64,
