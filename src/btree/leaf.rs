9fd39efd4eee46601b627f46dcd172b007c33c51
gettsu <tsuge-daisuke430@g.ecc.u-tokyo.ac.jp>
2022-02-08 21:17:54 +0900

    [add] slotted.rs

---
+++ src/btree/leaf.rs
@@ -0,0 +1,8 @@
+use std::mem::size_of;
+
+use zerocopy::{AsBytes, ByteSlice, ByteSliceMut, FromBytes, LayoutVerified};
+
+use super::Pair;
+use crate::bsearch::binary_search_by;
+use crate::disk::PageId;
+use crate::slotted::{self, Slotted};
